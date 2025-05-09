f0aabcf4960706525ed95a4ddfb9c7263b70f08a
Marvie-03 <kayveengee@gmail.com>
2025-05-08 20:01:11 -0700

    form update II

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -154,9 +154,20 @@ const Contact = () => {
                   <form
                     className="space-y-6"
                     data-netlify="true"
+                    data-netlify-honeypot="bot-field"
                     name="contact"
                     onSubmit={handleSubmit}
                   >
+                    {/* This hidden input is required for Netlify forms */}
+                    <input type="hidden" name="form-name" value="contact" />
+
+                    {/* Honeypot field to prevent spam */}
+                    <p className="hidden">
+                      <label>
+                        Don't fill this out if you're human:{" "}
+                        <input name="bot-field" />
+                      </label>
+                    </p>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                       <div>
                         <label
